bf9ef36fc3d888a3683078e5c3612f5e0431b451
ultrafro <hisham.bedri@gmail.com>
2018-08-29 00:45:58 -0400

    finished most of the prophet box, need to amend some of the search terms

---
+++ prophet_json_string.js
@@ -0,0 +1,202 @@
+﻿var prophet_json_string = [
+ {
+   "TransliteratedName": "Harun",
+   "EnglishName": "Aaron",
+   "TranslationName": "Harun",
+   "ArabicName": "هارون",
+   "Chronology": 15,
+   "Link": "https://en.wikipedia.org/wiki/Aaron#Islam"
+ },
+ {
+   "TransliteratedName": "Ibrahim",
+   "EnglishName": "Abraham",
+   "TranslationName": "Ibrahim",
+   "ArabicName": "إِبْـرَاهِـيـم",
+   "Chronology": 6,
+   "Link": "https://en.wikipedia.org/wiki/Abraham_in_Islam"
+ },
+ {
+   "TransliteratedName": "Adam",
+   "EnglishName": "Adam",
+   "TranslationName": "Adam",
+   "ArabicName": "آدم",
+   "Chronology": 1,
+   "Link": "https://en.wikipedia.org/wiki/Adam_in_Islam"
+ },
+ {
+   "TransliteratedName": "Dawud",
+   "EnglishName": "David",
+   "TranslationName": "Dawud",
+   "ArabicName": "دَاوود‎",
+   "Chronology": 17,
+   "Link": "https://en.wikipedia.org/wiki/David_in_Islam"
+ },
+ {
+   "TransliteratedName": "Ilyas",
+   "EnglishName": "Elijah",
+   "TranslationName": "Ilyas",
+   "ArabicName": "إِلـيـاس‎",
+   "Chronology": 19,
+   "Link": "https://en.wikipedia.org/wiki/Elijah#Elijah_in_Islam"
+ },
+ {
+   "TransliteratedName": "Al-Yasa",
+   "EnglishName": "Elisha",
+   "TranslationName": "Al-Yasa",
+   "ArabicName": "الـیَـسَـع‎",
+   "Chronology": 20,
+   "Link": "https://en.wikipedia.org/wiki/Elisha#In_Islam"
+ },
+ {
+   "TransliteratedName": "Idris",
+   "EnglishName": "Enoch",
+   "TranslationName": "Idris",
+   "ArabicName": "إِدريـس",
+   "Chronology": 2,
+   "Link": "https://en.wikipedia.org/wiki/Idris_(prophet)"
+ },
+ {
+   "TransliteratedName": "Dhul-Kifl",
+   "EnglishName": "Ezekiel",
+   "TranslationName": "Dhul-Kifl",
+   "ArabicName": "ذُو الـکِـفـل",
+   "Chronology": 16,
+   "Link": "https://en.wikipedia.org/wiki/Dhul-Kifl"
+ },
+ {
+   "TransliteratedName": "Hud",
+   "EnglishName": "Eber",
+   "TranslationName": "Hud",
+   "ArabicName": "هُـود",
+   "Chronology": 4,
+   "Link": "https://en.wikipedia.org/wiki/Hud_(prophet)"
+ },
+ {
+   "TransliteratedName": "Ishaq",
+   "EnglishName": "Isaac",
+   "TranslationName": "Ishaq",
+   "ArabicName": "إِسـحَـاق‎",
+   "Chronology": 9,
+   "Link": "https://en.wikipedia.org/wiki/Isaac_in_Islam"
+ },
+ {
+   "TransliteratedName": "Ismail",
+   "EnglishName": "Ishmael",
+   "TranslationName": "Ismail",
+   "ArabicName": "إِسـمَـاعِـيـل‎",
+   "Chronology": 8,
+   "Link": "https://en.wikipedia.org/wiki/Ishmael_in_Islam"
+ },
+ {
+   "TransliteratedName": "Yaqub",
+   "EnglishName": "Jacob",
+   "TranslationName": "Yaqub",
+   "ArabicName": "يَـعْـقُـوْب",
+   "Chronology": 10,
+   "Link": "https://en.wikipedia.org/wiki/Jacob_in_Islam"
+ },
+ {
+   "TransliteratedName": "Shuʿayb",
+   "EnglishName": "Jethro",
+   "TranslationName": "Shuʿayb",
+   "ArabicName": "شُـعـیـب‎",
+   "Chronology": 13,
+   "Link": "https://en.wikipedia.org/wiki/Shuaib"
+ },
+ {
+   "TransliteratedName": "Isa",
+   "EnglishName": "Jesus",
+   "TranslationName": "Isa",
+   "ArabicName": "عِـيـسَى",
+   "Chronology": 24,
+   "Link": "https://en.wikipedia.org/wiki/Jesus_in_Islam"
+ },
+ {
+   "TransliteratedName": "Ayub",
+   "EnglishName": "Job",
+   "TranslationName": "Ayub",
+   "ArabicName": "أَيّـوب‎",
+   "Chronology": 12,
+   "Link": "https://en.wikipedia.org/wiki/Job_in_Islam"
+ },
+ {
+   "TransliteratedName": "Yahya",
+   "EnglishName": "John the Baptist",
+   "TranslationName": "Yahya",
+   "ArabicName": "يَـحْـيَى ابْـن زَكَـرِيَّـا",
+   "Chronology": 23,
+   "Link": "https://en.wikipedia.org/wiki/John_the_Baptist#Islam"
+ },
+ {
+   "TransliteratedName": "Yusuf",
+   "EnglishName": "Joseph",
+   "TranslationName": "Yusuf",
+   "ArabicName": "يُـوسُـف ابـن يَـعـقُـوب",
+   "Chronology": 11,
+   "Link": "https://en.wikipedia.org/wiki/Joseph_in_Islam"
+ },
+ {
+   "TransliteratedName": "Yunus",
+   "EnglishName": "Jonah",
+   "TranslationName": "Yunus",
+   "ArabicName": "يُـونُـس",
+   "Chronology": 21,
+   "Link": "https://en.wikipedia.org/wiki/Jonah#Jonah_in_Islam"
+ },
+ {
+   "TransliteratedName": "Lut",
+   "EnglishName": "Lot",
+   "TranslationName": "Lut",
+   "ArabicName": "لُـوط",
+   "Chronology": 7,
+   "Link": "https://en.wikipedia.org/wiki/Lot_in_Islam"
+ },
+ {
+   "TransliteratedName": "Nuh",
+   "EnglishName": "Noah",
+   "TranslationName": "Nuh",
+   "ArabicName": "نُـوح",
+   "Chronology": 3,
+   "Link": "https://en.wikipedia.org/wiki/Noah_in_Islam"
+ },
+ {
+   "TransliteratedName": "Muhammad",
+   "EnglishName": "Muhammad",
+   "TranslationName": "Muhammad",
+   "ArabicName": "مُـحَـمَّـد",
+   "Chronology": 25,
+   "Link": "https://en.wikipedia.org/wiki/Muhammad_in_Islam"
+ },
+ {
+   "TransliteratedName": "Musa",
+   "EnglishName": "Moses",
+   "TranslationName": "Musa",
+   "ArabicName": "مُـوسَى",
+   "Chronology": 14,
+   "Link": "https://en.wikipedia.org/wiki/Moses_in_Islam"
+ },
+ {
+   "TransliteratedName": "Saleh",
+   "EnglishName": "Salah",
+   "TranslationName": "Saleh",
+   "ArabicName": "صَـالِـح",
+   "Chronology": 5,
+   "Link": "https://en.wikipedia.org/wiki/Saleh"
+ },
+ {
+   "TransliteratedName": "Sulayman",
+   "EnglishName": "Solomon",
+   "TranslationName": "Sulayman",
+   "ArabicName": "سُـلَـيـمَـان",
+   "Chronology": 18,
+   "Link": "https://en.wikipedia.org/wiki/Solomon_in_Islam"
+ },
+ {
+   "TransliteratedName": "Zakariya",
+   "EnglishName": "Zechariah",
+   "TranslationName": "Zakariya",
+   "ArabicName": "زَكَـرِيَّـا",
+   "Chronology": 22,
+   "Link": "https://en.wikipedia.org/wiki/Zechariah_(priest)#In_Islam"
+ }
+]
