e1cdc6837664a7fed56a921eb4d4c8e34011e591
ultrafro <hisham.bedri@gmail.com>
2018-08-31 02:09:52 -0400

    ugly 99 names box wiht english search

---
+++ names99_string.js
@@ -0,0 +1,695 @@
+﻿var names99_string = [
+ {
+   "Number": 1,
+   "Transliteration": "Ar Rahman",
+   "Arabic": "الرحمن",
+   "Translation": "The Most Gracious",
+   "SearchTerm": "The Most Gracious"
+ },
+ {
+   "Number": 2,
+   "Transliteration": "Ar Raheem",
+   "Arabic": "الرحيم",
+   "Translation": "The Most Merciful",
+   "SearchTerm": "The Most Merciful"
+ },
+ {
+   "Number": 3,
+   "Transliteration": "Al Malik",
+   "Arabic": "الملك",
+   "Translation": "The King",
+   "SearchTerm": "The King"
+ },
+ {
+   "Number": 4,
+   "Transliteration": "Al Quddus",
+   "Arabic": "القدوس",
+   "Translation": "The Most Holy",
+   "SearchTerm": "The Most Holy"
+ },
+ {
+   "Number": 5,
+   "Transliteration": "As Salam",
+   "Arabic": "السلام",
+   "Translation": "The Ultimate Provider of Peace",
+   "SearchTerm": "The Ultimate Provider of Peace"
+ },
+ {
+   "Number": 6,
+   "Transliteration": "Al Mu'min",
+   "Arabic": "المؤمن",
+   "Translation": "The Guardian of Faith",
+   "SearchTerm": "The Guardian of Faith"
+ },
+ {
+   "Number": 7,
+   "Transliteration": "Al Muhaymin",
+   "Arabic": "المهيمن",
+   "Translation": "The Guardian, the Preserver",
+   "SearchTerm": "The Guardian, the Preserver"
+ },
+ {
+   "Number": 8,
+   "Transliteration": "Al Aziz",
+   "Arabic": "العزيز",
+   "Translation": "The Almighty, the Self Sufficient",
+   "SearchTerm": "The Almighty, the Self Sufficient"
+ },
+ {
+   "Number": 9,
+   "Transliteration": "Al Jabbaar",
+   "Arabic": "الجبار",
+   "Translation": "The Compeller",
+   "SearchTerm": "The Compeller"
+ },
+ {
+   "Number": 10,
+   "Transliteration": "Al Mutakabbir",
+   "Arabic": "الْمُتَكَبِّرُ",
+   "Translation": "The Dominant one",
+   "SearchTerm": "The Dominant one"
+ },
+ {
+   "Number": 11,
+   "Transliteration": "Al Khaaliq",
+   "Arabic": "الخالق",
+   "Translation": "The Creator",
+   "SearchTerm": "The Creator"
+ },
+ {
+   "Number": 12,
+   "Transliteration": "Al Baari",
+   "Arabic": "البارئ",
+   "Translation": "The Maker",
+   "SearchTerm": "The Maker"
+ },
+ {
+   "Number": 13,
+   "Transliteration": "Al Musawwir",
+   "Arabic": "المصور",
+   "Translation": "The Fashioner of Forms",
+   "SearchTerm": "The Fashioner of Forms"
+ },
+ {
+   "Number": 14,
+   "Transliteration": "Al Ghaffaar",
+   "Arabic": "الغفار",
+   "Translation": "The Ever-Forgiving",
+   "SearchTerm": "The Ever-Forgiving"
+ },
+ {
+   "Number": 15,
+   "Transliteration": "Al Qahhaar",
+   "Arabic": "القهار",
+   "Translation": "The All Subduer",
+   "SearchTerm": "The All Subduer"
+ },
+ {
+   "Number": 16,
+   "Transliteration": "Al Wahhaab",
+   "Arabic": "الوهاب",
+   "Translation": "The Bestower",
+   "SearchTerm": "The Bestower"
+ },
+ {
+   "Number": 17,
+   "Transliteration": "Ar Razzaaq",
+   "Arabic": "الرزاق",
+   "Translation": "The Ever-Providing, The Sustainer",
+   "SearchTerm": "The Ever-Providing, The Sustainer"
+ },
+ {
+   "Number": 18,
+   "Transliteration": "Al Fattaah",
+   "Arabic": "الفتاح",
+   "Translation": "The Ultimate Judge, The Opener of All Portals, the Victory Giver",
+   "SearchTerm": "The Ultimate Judge, The Opener of All Portals, the Victory Giver"
+ },
+ {
+   "Number": 19,
+   "Transliteration": "Al Alim",
+   "Arabic": "العليم",
+   "Translation": "The All-Knowing, the Omniscient",
+   "SearchTerm": "The All-Knowing, the Omniscient"
+ },
+ {
+   "Number": 20,
+   "Transliteration": "Al Qaabidh",
+   "Arabic": "القابض",
+   "Translation": "The Restrainer, the Straightener",
+   "SearchTerm": "The Restrainer, the Straightener"
+ },
+ {
+   "Number": 21,
+   "Transliteration": "Al Baasit",
+   "Arabic": "الباسط",
+   "Translation": "The Expander, the Munificent",
+   "SearchTerm": "The Expander, the Munificent"
+ },
+ {
+   "Number": 22,
+   "Transliteration": "Al Khaafidh",
+   "Arabic": "الخافض",
+   "Translation": "The Abaser, The Demeanor",
+   "SearchTerm": "The Abaser, The Demeanor"
+ },
+ {
+   "Number": 23,
+   "Transliteration": "Ar Raafi'",
+   "Arabic": "الرافع",
+   "Translation": "The Exalter",
+   "SearchTerm": "The Exalter"
+ },
+ {
+   "Number": 24,
+   "Transliteration": "Al Mu'izz",
+   "Arabic": "المعز",
+   "Translation": "The Giver of Honour",
+   "SearchTerm": "The Giver of Honour"
+ },
+ {
+   "Number": 25,
+   "Transliteration": "Al Muzil",
+   "Arabic": "المذل",
+   "Translation": "The Giver of Dishonor",
+   "SearchTerm": "The Giver of Dishonor"
+ },
+ {
+   "Number": 26,
+   "Transliteration": "As Sami'",
+   "Arabic": "السميع",
+   "Translation": "The All-Hearing",
+   "SearchTerm": "The All-Hearing"
+ },
+ {
+   "Number": 27,
+   "Transliteration": "Al Basir",
+   "Arabic": "البصير",
+   "Translation": "The All-Seeing",
+   "SearchTerm": "The All-Seeing"
+ },
+ {
+   "Number": 28,
+   "Transliteration": "Al Hakam",
+   "Arabic": "الحكم",
+   "Translation": "The Judge, the Ultimate Arbiter",
+   "SearchTerm": "The Judge, the Ultimate Arbiter"
+ },
+ {
+   "Number": 29,
+   "Transliteration": "Al 'Adl",
+   "Arabic": "العدل",
+   "Translation": "The Utterly Just",
+   "SearchTerm": "The Utterly Just"
+ },
+ {
+   "Number": 30,
+   "Transliteration": "Al Latif",
+   "Arabic": "اللطيف",
+   "Translation": "The Kind",
+   "SearchTerm": "The Kind"
+ },
+ {
+   "Number": 31,
+   "Transliteration": "Al Khabir",
+   "Arabic": "الخبير",
+   "Translation": "The All-Aware",
+   "SearchTerm": "The All-Aware"
+ },
+ {
+   "Number": 32,
+   "Transliteration": "Al Halim",
+   "Arabic": "الحليم",
+   "Translation": "The Forbearer, The Indulgent",
+   "SearchTerm": "The Forbearer, The Indulgent"
+ },
+ {
+   "Number": 33,
+   "Transliteration": "Al-‘Adheem",
+   "Arabic": "العظيم",
+   "Translation": "The Magnificent, the Infinite",
+   "SearchTerm": "The Magnificent, the Infinite"
+ },
+ {
+   "Number": 34,
+   "Transliteration": "Al Ghafur",
+   "Arabic": "الغفور",
+   "Translation": "The All-Forgiving",
+   "SearchTerm": "The All-Forgiving"
+ },
+ {
+   "Number": 35,
+   "Transliteration": "Ash Shakur",
+   "Arabic": "الشكور",
+   "Translation": "The Grateful",
+   "SearchTerm": "The Grateful"
+ },
+ {
+   "Number": 36,
+   "Transliteration": "Al Ali",
+   "Arabic": "العلي",
+   "Translation": "The Sublimely Exalted",
+   "SearchTerm": "The Sublimely Exalted"
+ },
+ {
+   "Number": 37,
+   "Transliteration": "Al Kabir",
+   "Arabic": "الكبير",
+   "Translation": "The Great",
+   "SearchTerm": "The Great"
+ },
+ {
+   "Number": 38,
+   "Transliteration": "Al Hafidh",
+   "Arabic": "الحفيظ",
+   "Translation": "The Preserver, The Protector",
+   "SearchTerm": "The Preserver, The Protector"
+ },
+ {
+   "Number": 39,
+   "Transliteration": "Al Muqit",
+   "Arabic": "المقيت",
+   "Translation": "The Nourisher",
+   "SearchTerm": "The Nourisher"
+ },
+ {
+   "Number": 40,
+   "Transliteration": "Al Hasib",
+   "Arabic": "الحسيب",
+   "Translation": "The Reckoner",
+   "SearchTerm": "The Reckoner"
+ },
+ {
+   "Number": 41,
+   "Transliteration": "Al Jalil",
+   "Arabic": "الجليل",
+   "Translation": "The Majestic",
+   "SearchTerm": "The Majestic"
+ },
+ {
+   "Number": 42,
+   "Transliteration": "Al Karim",
+   "Arabic": "الكريم",
+   "Translation": "The Bountiful, the Generous",
+   "SearchTerm": "The Bountiful, the Generous"
+ },
+ {
+   "Number": 43,
+   "Transliteration": "Ar Raqib",
+   "Arabic": "الرقيب",
+   "Translation": "The Watchful",
+   "SearchTerm": "The Watchful"
+ },
+ {
+   "Number": 44,
+   "Transliteration": "Al Mujib",
+   "Arabic": "المجيب",
+   "Translation": "The Responsive, the Answerer",
+   "SearchTerm": "The Responsive, the Answerer"
+ },
+ {
+   "Number": 45,
+   "Transliteration": "Al Wasi'",
+   "Arabic": "الواسع",
+   "Translation": "The Vast, the All Encompassing",
+   "SearchTerm": "The Vast, the All Encompassing"
+ },
+ {
+   "Number": 46,
+   "Transliteration": "Al Hakim",
+   "Arabic": "الحكيم",
+   "Translation": "The Wise",
+   "SearchTerm": "The Wise"
+ },
+ {
+   "Number": 47,
+   "Transliteration": "Al Wadud",
+   "Arabic": "الودود",
+   "Translation": "The Loving, the Kind One",
+   "SearchTerm": "The Loving, the Kind One"
+ },
+ {
+   "Number": 48,
+   "Transliteration": "Al Majid",
+   "Arabic": "المجيد",
+   "Translation": "The All Glorious",
+   "SearchTerm": "The All Glorious"
+ },
+ {
+   "Number": 49,
+   "Transliteration": "Al Ba'ith",
+   "Arabic": "الباعث",
+   "Translation": "The Raiser of the Dead",
+   "SearchTerm": "The Raiser of the Dead"
+ },
+ {
+   "Number": 50,
+   "Transliteration": "Ash Shaheed",
+   "Arabic": "الشهيد",
+   "Translation": "The Witness",
+   "SearchTerm": "The Witness"
+ },
+ {
+   "Number": 51,
+   "Transliteration": "Al Haqq",
+   "Arabic": "الحق",
+   "Translation": "The Truth, the Real",
+   "SearchTerm": "The Truth, the Real"
+ },
+ {
+   "Number": 52,
+   "Transliteration": "Al Wakil",
+   "Arabic": "الوكيل",
+   "Translation": "The Trustee, the Dependable",
+   "SearchTerm": "The Trustee, the Dependable"
+ },
+ {
+   "Number": 53,
+   "Transliteration": "Al Qawiyy",
+   "Arabic": "القوي",
+   "Translation": "The Strong",
+   "SearchTerm": "The Strong"
+ },
+ {
+   "Number": 54,
+   "Transliteration": "Al Mateen",
+   "Arabic": "المتين",
+   "Translation": "The Firm, the Steadfast",
+   "SearchTerm": "The Firm, the Steadfast"
+ },
+ {
+   "Number": 55,
+   "Transliteration": "Al Wali",
+   "Arabic": "الولي",
+   "Translation": "The Protecting Friend, Patron, and Supporter",
+   "SearchTerm": "The Protecting Friend, Patron, and Supporter"
+ },
+ {
+   "Number": 56,
+   "Transliteration": "Al Hamidu",
+   "Arabic": "الحميد",
+   "Translation": "The All Praise Worthy",
+   "SearchTerm": "The All Praise Worthy"
+ },
+ {
+   "Number": 57,
+   "Transliteration": "Al Muhsi",
+   "Arabic": "المحصي",
+   "Translation": "The Accounter, The Numberer of All",
+   "SearchTerm": "The Accounter, The Numberer of All"
+ },
+ {
+   "Number": 58,
+   "Transliteration": "Al Mubdi",
+   "Arabic": "المبدئ",
+   "Translation": "The Producer, Originator, and Initiator of all",
+   "SearchTerm": "The Producer, Originator, and Initiator of all"
+ },
+ {
+   "Number": 59,
+   "Transliteration": "Al Mu'id",
+   "Arabic": "المعيد",
+   "Translation": "The Reinstater Who Brings Back All",
+   "SearchTerm": "The Reinstater Who Brings Back All"
+ },
+ {
+   "Number": 60,
+   "Transliteration": "Al Muhyi",
+   "Arabic": "المحيي",
+   "Translation": "The Giver of Life",
+   "SearchTerm": "The Giver of Life"
+ },
+ {
+   "Number": 61,
+   "Transliteration": "Al Mumit",
+   "Arabic": "المميت",
+   "Translation": "The Bringer of Death, the Destroyer",
+   "SearchTerm": "The Bringer of Death, the Destroyer"
+ },
+ {
+   "Number": 62,
+   "Transliteration": "Al Hayy",
+   "Arabic": "الحي",
+   "Translation": "The Ever Living",
+   "SearchTerm": "The Ever Living"
+ },
+ {
+   "Number": 63,
+   "Transliteration": "Al Qayyum",
+   "Arabic": "القيوم",
+   "Translation": "The Self Subsisting Sustainer of All",
+   "SearchTerm": "The Self Subsisting Sustainer of All"
+ },
+ {
+   "Number": 64,
+   "Transliteration": "Al Waajid",
+   "Arabic": "الواجد",
+   "Translation": "The Perceiver, the Finder, the Unfailing",
+   "SearchTerm": "The Perceiver, the Finder, the Unfailing"
+ },
+ {
+   "Number": 65,
+   "Transliteration": "Al Maajid",
+   "Arabic": "الماجد",
+   "Translation": "The Illustrious, the Magnificent",
+   "SearchTerm": "The Illustrious, the Magnificent"
+ },
+ {
+   "Number": 66,
+   "Transliteration": "Al Waahid",
+   "Arabic": "الواحد",
+   "Translation": "The One, the All Inclusive, the Indivisible",
+   "SearchTerm": "The One, the All Inclusive, the Indivisible"
+ },
+ {
+   "Number": 67,
+   "Transliteration": "Al Ahad",
+   "Arabic": "الاحد",
+   "Translation": "The One, the Indivisible",
+   "SearchTerm": "The One, the Indivisible"
+ },
+ {
+   "Number": 68,
+   "Transliteration": "As Samad",
+   "Arabic": "الصمد",
+   "Translation": "The Everlasting,The Eternal Refuge",
+   "SearchTerm": "The Everlasting,The Eternal Refuge"
+ },
+ {
+   "Number": 69,
+   "Transliteration": "Al Qaadir",
+   "Arabic": "القادر",
+   "Translation": "The All-Capable, The Most Able, The Most Powerful",
+   "SearchTerm": "The All-Capable, The Most Able, The Most Powerful"
+ },
+ {
+   "Number": 70,
+   "Transliteration": "Al Muqtadir",
+   "Arabic": "المقتدر",
+   "Translation": "The All Determiner, the Dominant",
+   "SearchTerm": "The All Determiner, the Dominant"
+ },
+ {
+   "Number": 71,
+   "Transliteration": "Al Muqaddim",
+   "Arabic": "المقدم",
+   "Translation": "The Expediter, He who brings forward",
+   "SearchTerm": "The Expediter, He who brings forward"
+ },
+ {
+   "Number": 72,
+   "Transliteration": "Al Mu'akhkhir",
+   "Arabic": "المؤخر",
+   "Translation": "The Delayer, He who brings backwards",
+   "SearchTerm": "The Delayer, He who brings backwards"
+ },
+ {
+   "Number": 73,
+   "Transliteration": "Al Awwal",
+   "Arabic": "الأول",
+   "Translation": "The First",
+   "SearchTerm": "The First"
+ },
+ {
+   "Number": 74,
+   "Transliteration": "Al Aakhir",
+   "Arabic": "الآخر",
+   "Translation": "The Last",
+   "SearchTerm": "The Last"
+ },
+ {
+   "Number": 75,
+   "Transliteration": "Az Dhaahir",
+   "Arabic": "الظاهر",
+   "Translation": "The Manifest; the All Victorious",
+   "SearchTerm": "The Manifest; the All Victorious"
+ },
+ {
+   "Number": 76,
+   "Transliteration": "Al Baatin",
+   "Arabic": "الباطن",
+   "Translation": "The Hidden; the All Encompassing",
+   "SearchTerm": "The Hidden; the All Encompassing"
+ },
+ {
+   "Number": 77,
+   "Transliteration": "Al Waali",
+   "Arabic": "الوالي",
+   "Translation": "The Patron",
+   "SearchTerm": "The Patron"
+ },
+ {
+   "Number": 78,
+   "Transliteration": "Al Muta'ali",
+   "Arabic": "المتعالي",
+   "Translation": "The Self Exalted",
+   "SearchTerm": "The Self Exalted"
+ },
+ {
+   "Number": 79,
+   "Transliteration": "Al Barr",
+   "Arabic": "البر",
+   "Translation": "The Most Kind and Righteous",
+   "SearchTerm": "The Most Kind and Righteous"
+ },
+ {
+   "Number": 80,
+   "Transliteration": "At Tawwaab",
+   "Arabic": "التواب",
+   "Translation": "The Ever-Pardoning, Ever Relenting",
+   "SearchTerm": "The Ever-Pardoning, Ever Relenting"
+ },
+ {
+   "Number": 81,
+   "Transliteration": "Al Muntaqim",
+   "Arabic": "المنتقم",
+   "Translation": "The Avenger",
+   "SearchTerm": "The Avenger"
+ },
+ {
+   "Number": 82,
+   "Transliteration": "Al 'Afuww",
+   "Arabic": "العفو",
+   "Translation": "The Pardoner, The Forgiver",
+   "SearchTerm": "The Pardoner, The Forgiver"
+ },
+ {
+   "Number": 83,
+   "Transliteration": "Ar Ra'uf",
+   "Arabic": "الرؤوف",
+   "Translation": "The Clement, The Compassionate, The All-Pitying",
+   "SearchTerm": "The Clement, The Compassionate, The All-Pitying"
+ },
+ {
+   "Number": 84,
+   "Transliteration": "Malik Al Mulk",
+   "Arabic": "مالك الملك",
+   "Translation": "The Owner of All Sovereignty",
+   "SearchTerm": "The Owner of All Sovereignty"
+ },
+ {
+   "Number": 85,
+   "Transliteration": "Dhual Jalal wa Al Ikram",
+   "Arabic": "ذو الجلال و الإكرام",
+   "Translation": "The Lord of Majesty and Generosity",
+   "SearchTerm": "The Lord of Majesty and Generosity"
+ },
+ {
+   "Number": 86,
+   "Transliteration": "Al Muqsit",
+   "Arabic": "المقسط",
+   "Translation": "The Equitable, the Requiter",
+   "SearchTerm": "The Equitable, the Requiter"
+ },
+ {
+   "Number": 87,
+   "Transliteration": "Al Jaami'",
+   "Arabic": "الجامع",
+   "Translation": "The Gatherer, the Unifier",
+   "SearchTerm": "The Gatherer, the Unifier"
+ },
+ {
+   "Number": 88,
+   "Transliteration": "Al Ghani",
+   "Arabic": "الغني",
+   "Translation": "The All Rich, the Independent",
+   "SearchTerm": "The All Rich, the Independent"
+ },
+ {
+   "Number": 89,
+   "Transliteration": "Al Mughni",
+   "Arabic": "المغني",
+   "Translation": "The Enricher, the Emancipator",
+   "SearchTerm": "The Enricher, the Emancipator"
+ },
+ {
+   "Number": 90,
+   "Transliteration": "Al Mani'",
+   "Arabic": "المانع",
+   "Translation": "The Withholder, the Shielder, the Defender",
+   "SearchTerm": "The Withholder, the Shielder, the Defender"
+ },
+ {
+   "Number": 91,
+   "Transliteration": "Ad Dharr",
+   "Arabic": "الضآر",
+   "Translation": "The Distresser",
+   "SearchTerm": "The Distresser"
+ },
+ {
+   "Number": 92,
+   "Transliteration": "An Nafi'",
+   "Arabic": "النافع",
+   "Translation": "The Propitious, the Benefactor",
+   "SearchTerm": "The Propitious, the Benefactor"
+ },
+ {
+   "Number": 93,
+   "Transliteration": "An Nur",
+   "Arabic": "النور",
+   "Translation": "The Light",
+   "SearchTerm": "The Light"
+ },
+ {
+   "Number": 94,
+   "Transliteration": "Al Hadi",
+   "Arabic": "الهادي",
+   "Translation": "The Guide",
+   "SearchTerm": "The Guide"
+ },
+ {
+   "Number": 95,
+   "Transliteration": "Al Badi'i",
+   "Arabic": "البديع",
+   "Translation": "Incomparable, the Originator",
+   "SearchTerm": "Incomparable, the Originator"
+ },
+ {
+   "Number": 96,
+   "Transliteration": "Al Baaqi",
+   "Arabic": "الباقي",
+   "Translation": "The Ever Enduring and Immutable",
+   "SearchTerm": "The Ever Enduring and Immutable"
+ },
+ {
+   "Number": 97,
+   "Transliteration": "Al Waarith",
+   "Arabic": "الوارث",
+   "Translation": "The Heir, the Inheritor of All",
+   "SearchTerm": "The Heir, the Inheritor of All"
+ },
+ {
+   "Number": 98,
+   "Transliteration": "Ar Rashid",
+   "Arabic": "الرشيد",
+   "Translation": "The Guide, Infallible Teacher, and Knower",
+   "SearchTerm": "The Guide, Infallible Teacher, and Knower"
+ },
+ {
+   "Number": 99,
+   "Transliteration": "Al Saboor",
+   "Arabic": "الصبور",
+   "Translation": "The Forbearing, The Patient",
+   "SearchTerm": "The Forbearing, The Patient"
+ }
+]
